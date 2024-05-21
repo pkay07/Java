17f0a9db4e36f704855cd9f36aaec11980f021b7
Prakash Navin <thebeastpkay@gmail.com>
2024-05-21 13:18:14 +0530

    Very first Commit of java files

---
+++ src/Bas/Hello.java
@@ -0,0 +1,9 @@
+package Bas;
+
+public class Hello
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("kaisa hai chal firse suru karte hai coding");
+    }
+}
